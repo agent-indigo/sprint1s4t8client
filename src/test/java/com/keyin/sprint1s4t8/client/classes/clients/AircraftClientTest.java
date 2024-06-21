78c7f751c4922373e33c96bfe05b02c7fe8c4cab
Manny-Nwokedi <131998414+Manny-Nwokedi@users.noreply.github.com>
2024-06-21 21:27:59 -0230

    Update

--- src/test/java/com/keyin/sprint1s4t8/client/classes/clients/AircraftClientTest.java
+++ src/test/java/com/keyin/sprint1s4t8/client/classes/clients/AircraftClientTest.java
@@ -1,5 +1,22 @@
-package com.keyin.sprint1s4t8.client.classes.clients;
+package com.keyin.sprint1s4t8.client;
 
-public final class AircraftClientTest {
+import com.keyin.sprint1s4t8.client.classes.models.AircraftModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+public class AircraftClientTest {
+    @Test
+    public void testBuildAircraftListFromResponse() throws Exception {
+        String jsonResponse = "[\n{\n\"id\": 1,\n\"model\": \"Boeing 737\",\n\"airline\": \"WestJet\",\n\"capacity\": 200\n},\n" +
+                "{\n\"id\": 2,\n\"model\": \"Airbus A320\",\n\"airline\": \"Air Canada\",\n\"capacity\": 180\n}\n]";
+
+        AircraftClient aircraftClientUnderTest = new AircraftClient();
+
+        List<AircraftModel> aircraftList = aircraftClientUnderTest.buildAircraftListFromResponse(jsonResponse);
+
+        Assertions.assertTrue(aircraftList.stream().anyMatch(a -> a.getModel().equals("Boeing 737")));
+        Assertions.assertTrue(aircraftList.stream().anyMatch(a -> a.getModel().equals("Airbus A320")));
+    }
 }
