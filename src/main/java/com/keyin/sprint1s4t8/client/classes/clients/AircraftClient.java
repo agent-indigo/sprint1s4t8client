0b1aa453c7bbf678b428727752325005fdfbd757
MohamedMaghrebi <138633077+MohamedMaghrebi@users.noreply.github.com>
2024-06-21 21:33:21 -0230

    Add New AircraftCLI

--- src/main/java/com/keyin/sprint1s4t8/client/classes/clients/AircraftClient.java
+++ src/main/java/com/keyin/sprint1s4t8/client/classes/clients/AircraftClient.java
@@ -1,4 +1,39 @@
 package com.keyin.sprint1s4t8.client.classes.clients;
 
+import com.keyin.sprint1s4t8.client.classes.models.AircraftModel;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AircraftClient {
+
+    public List<AircraftModel> getAllAirports() {
+        List<AircraftModel> airports = new ArrayList<AircraftModel>();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create('http;//localhost:8080')).build();
+
+        try {
+            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode()==200) {
+                System.out.println("***** " + response.body());
+            } else {
+                System.out.println("Error Status Code: " + response.statusCode());
+            }
+
+            airports = buildAirportListFromResponse(response.body());
+
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        return airports;
+    }
+
 }
--- src/main/java/com/keyin/sprint1s4t8/client/classes/clis/AircraftCLI.java
+++ src/main/java/com/keyin/sprint1s4t8/client/classes/clis/AircraftCLI.java
@@ -1,4 +1,96 @@
-package com.keyin.sprint1s4t8.client.classes.clis;
+//package com.keyin.sprint1s4t8.client.classes.clis;
+//
+//import com.keyin.sprint1s4t8.client.classes.clients.AircraftClient;
+//import com.keyin.sprint1s4t8.client.classes.models.AircraftModel;
+//
+//import java.util.List;
+//
+//public class HTTPRestCLIApplication {
+//
+//    private AircraftClient aircraftClient;
+//
+//    public AircraftClient getAircraftClient() {
+//        return aircraftClient;
+//    }
+//
+//    public void setAircraftClient(AircraftClient aircraftClient) {
+//        this.aircraftClient = aircraftClient;
+//    }
+//
+//    public void getAllAircrafts(){
+//        List<AircraftModel> aircraftModels = getAircraftClient().getAllAircrafts();
+//    }
+//
+//
+//}
+//// Review Jamies Project Structure For CLIs. Need only one
+
+
+package com.keyin.sprint1s4t8.client.cli;
+
+import com.keyin.sprint1s4t8.client.AircraftClient;
+import com.keyin.sprint1s4t8.client.classes.models.AircraftModel;
+
+import java.util.List;
 
 public class AircraftCLI {
+    private AircraftClient aircraftClient;
+
+    public String generateAircraftReport() {
+        List<AircraftModel> aircrafts = getAircraftClient().getAllAircrafts();
+
+        StringBuffer report = new StringBuffer();
+
+        for (AircraftModel aircraft : aircrafts) {
+            report.append(aircraft.getModel());
+            report.append(" - ");
+            report.append(aircraft.getAirline());
+            report.append(" - ");
+            report.append(aircraft.getCapacity());
+
+            if (aircrafts.indexOf(aircraft) != (aircrafts.size() - 1)) {
+                report.append(", ");
+            }
+        }
+
+        System.out.println(report.toString());
+
+        return report.toString();
+    }
+
+    private void listAircrafts() {
+        System.out.println(getAircraftClient().getResponseFromHTTPRequest());
+    }
+
+    public AircraftClient getAircraftClient() {
+        if (aircraftClient == null) {
+            aircraftClient = new AircraftClient();
+        }
+
+        return aircraftClient;
+    }
+
+    public void setAircraftClient(AircraftClient aircraftClient) {
+        this.aircraftClient = aircraftClient;
+    }
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+        AircraftCLI cliApp = new AircraftCLI();
+
+        String serverURL = args[0];
+
+        if (serverURL != null && !serverURL.isEmpty()) {
+
+            AircraftClient aircraftClient = new AircraftClient();
+            aircraftClient.setServerURL(serverURL);
+
+            cliApp.setAircraftClient(aircraftClient);
+
+            cliApp.generateAircraftReport();
+        }
+    }
 }
