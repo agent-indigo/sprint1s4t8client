2c91f351b8c3bf40430202f378dd9afe3d5d91b4
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-06-24 14:18:36 -0230

    Fix files

--- src/main/java/com/keyin/sprint1s4t8/client/classes/App.java
+++ src/main/java/com/keyin/sprint1s4t8/client/classes/App.java
@@ -1,5 +1,10 @@
 package com.keyin.sprint1s4t8.client.classes;
+
+import com.keyin.sprint1s4t8.client.classes.clients.AircraftClient;
+import com.keyin.sprint1s4t8.client.classes.clis.AircraftCLI;
+
 public final class App {
     public App() {}
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 }
