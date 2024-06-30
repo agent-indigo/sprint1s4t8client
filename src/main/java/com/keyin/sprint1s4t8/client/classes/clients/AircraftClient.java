a7284a6fa8148e17e7e806d84db951145501f4f6
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-06-30 17:19:36 -0230

    I fixed the AircraftCLI.java and AircraftClient.java
    I managed to add a functionality of adding airports and passengers to the aircraft client and aircraft CLI files.

--- src/main/java/com/keyin/sprint1s4t8/client/classes/clients/AircraftClient.java
+++ src/main/java/com/keyin/sprint1s4t8/client/classes/clients/AircraftClient.java
@@ -3,17 +3,22 @@ package com.keyin.sprint1s4t8.client.classes.clients;
 
 import com.keyin.sprint1s4t8.client.classes.abstracts.Client;
 import com.keyin.sprint1s4t8.client.classes.models.AircraftModel;
+import com.keyin.sprint1s4t8.client.classes.models.AirportModel;
+import com.keyin.sprint1s4t8.client.classes.models.PassengerModel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
 
 public final class AircraftClient extends Client {
     private AircraftModel aircraft;
     private List<AircraftModel> aircrafts;
+    private LinkedList<AirportModel> airports;
+    private LinkedList<PassengerModel> passengers;
 
     public AircraftClient() {
         super("aircrafts");
@@ -65,6 +70,108 @@ public final class AircraftClient extends Client {
             connection.setRequestMethod("PUT");
             connection.setRequestProperty("Content-Type", "application/json");
             connection.getOutputStream().write(mapper.writeValueAsString(update).getBytes());
+            return connection.getResponseMessage().toString();
+        } catch (IOException ioException) {
+            return ioException.getStackTrace().toString();
+        }
+    }
+
+    public LinkedList<AirportModel> listAirports(int id) {
+        try {
+            for (AircraftModel aircraft : list()) {
+                if (aircraft.getId() == id) {
+                    this.airports = mapper.convertValue(aircraft.getAirports(), new TypeReference<LinkedList<AirportModel>>() {});
+                }
+            }
+            return airports;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            illegalArgumentException.printStackTrace();
+            return null;
+        }
+    }
+
+    public String addAirport(int id, AirportModel airport) {
+        this.address = address + id + "/airports";
+        try {
+            for (AircraftModel aircraft : list()) {
+                if (aircraft.getId() == id) {
+                    this.url = URL.of(uri, null);
+                    this.connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoOutput(true);
+                    connection.setDoInput(true);
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.getOutputStream().write(mapper.writeValueAsString(airport).getBytes());
+                }
+            }
+            return connection.getResponseMessage().toString();
+        } catch (IOException ioException) {
+            return ioException.getStackTrace().toString();
+        }
+    }
+
+    public String deleteAirport(int id, int index) {
+        this.address = address + id + "/airports/" + index;
+        try {
+            for (AircraftModel aircraft : list()) {
+                if (aircraft.getId() == id) {
+                    this.url = URL.of(uri, null);
+                    this.connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoOutput(true);
+                    connection.setRequestMethod("DELETE");
+                }
+            }
+            return connection.getResponseMessage().toString();
+        } catch (IOException ioException) {
+            return ioException.getStackTrace().toString();
+        }
+    }
+
+    public LinkedList<PassengerModel> listPassengers(int id) {
+        try {
+            for (AircraftModel aircraft : list()) {
+                if (aircraft.getId() == id) {
+                    this.passengers = mapper.convertValue(aircraft.getPassengers(), new TypeReference<LinkedList<PassengerModel>>() {});
+                }
+            }
+            return passengers;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            illegalArgumentException.printStackTrace();
+            return null;
+        }
+    }
+
+    public String addPassenger(int id, PassengerModel passenger) {
+        this.address = address + id + "/passengers";
+        try {
+            for (AircraftModel aircraft : list()) {
+                if (aircraft.getId() == id) {
+                    this.url = URL.of(uri, null);
+                    this.connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoOutput(true);
+                    connection.setDoInput(true);
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.getOutputStream().write(mapper.writeValueAsString(passenger).getBytes());
+                }
+            }
+            return connection.getResponseMessage().toString();
+        } catch (IOException ioException) {
+            return ioException.getStackTrace().toString();
+        }
+    }
+
+    public String deletePassenger(int id, int index) {
+        this.address = address + id + "/passengers/" + index;
+        try {
+            for (AircraftModel aircraft : list()) {
+                if (aircraft.getId() == id) {
+                    this.url = URL.of(uri, null);
+                    this.connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoOutput(true);
+                    connection.setRequestMethod("DELETE");
+                }
+            }
             return connection.getResponseMessage().toString();
         } catch (IOException ioException) {
             return ioException.getStackTrace().toString();
--- src/main/java/com/keyin/sprint1s4t8/client/classes/clis/AircraftCLI.java
+++ src/main/java/com/keyin/sprint1s4t8/client/classes/clis/AircraftCLI.java
@@ -2,15 +2,20 @@
 package com.keyin.sprint1s4t8.client.classes.clis;
 
 import com.keyin.sprint1s4t8.client.classes.models.AircraftModel;
+import com.keyin.sprint1s4t8.client.classes.models.AirportModel;
+import com.keyin.sprint1s4t8.client.classes.models.PassengerModel;
 import com.keyin.sprint1s4t8.client.classes.abstracts.CLI;
 import com.keyin.sprint1s4t8.client.classes.clients.AircraftClient;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public final class AircraftCLI extends CLI {
     private AircraftClient client;
     private AircraftModel aircraft;
     private List<AircraftModel> aircrafts;
+    private LinkedList<AirportModel> airports;
+    private LinkedList<PassengerModel> passengers;
 
     public AircraftCLI() {
         super();
@@ -26,8 +31,6 @@ public final class AircraftCLI extends CLI {
             response.append(aircraft.getModel());
             response.append(",");
             response.append(aircraft.getAirline());
-            response.append(",");
-            response.append(aircraft.getCapacity());
             if (aircrafts.indexOf(aircraft) < aircrafts.size()) response.append("\n");
         }
         return response.toString();
@@ -41,8 +44,6 @@ public final class AircraftCLI extends CLI {
         response.append(aircraft.getModel());
         response.append(",");
         response.append(aircraft.getAirline());
-        response.append(",");
-        response.append(aircraft.getCapacity());
         if (aircrafts.indexOf(aircraft) < aircrafts.size()) response.append("\n");
         return response.toString();
     }
@@ -58,5 +59,63 @@ public final class AircraftCLI extends CLI {
     @Override
     public void delete(int id) {
         client.delete(id);
+    }
+
+    public String listAirports(int id) {
+        for (AircraftModel aircraft : aircrafts) {
+            if (aircraft.getId() == id) {
+                this.airports = aircraft.getAirports();
+                for (AirportModel airport : airports) {
+                    response.append(airport.getId());
+                    response.append(",");
+                    response.append(airport.getName());
+                    response.append(",");
+                    response.append(airport.getCode());
+                    if (airports.indexOf(airport) < airports.size()) response.append("\n");
+                }
+            }
+        }
+        return response.toString();
+    }
+
+    public void addAirport(int id, AirportModel airport) {
+        for (AircraftModel aircraft : aircrafts) {
+            if (aircraft.getId() == id) client.addAirport(id, airport);
+        }
+    }
+
+    public void deleteAirport(int id, int index) {
+        for (AircraftModel aircraft : aircrafts) {
+            if (aircraft.getId() == id) client.deleteAirport(id, index);
+        }
+    }
+
+    public String listPassengers(int id) {
+        for (AircraftModel aircraft : aircrafts) {
+            if (aircraft.getId() == id) {
+                this.passengers = aircraft.getPassengers();
+                for (PassengerModel passenger : passengers) {
+                    response.append(passenger.getId());
+                    response.append(",");
+                    response.append(passenger.getFirst());
+                    response.append(",");
+                    response.append(passenger.getLast());
+                    if (passengers.indexOf(passenger) < passengers.size()) response.append("\n");
+                }
+            }
+        }
+        return response.toString();
+    }
+
+    public void addPassenger(int id, PassengerModel passenger) {
+        for (AircraftModel aircraft : aircrafts) {
+            if (aircraft.getId() == id) client.addPassenger(id, passenger);
+        }
+    }
+
+    public void deletePassenger(int id, int index) {
+        for (AircraftModel aircraft : aircrafts) {
+            if (aircraft.getId() == id) client.deletePassenger(id, index);
+        }
     }
 }
